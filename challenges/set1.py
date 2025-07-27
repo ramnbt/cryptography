8829b7b9cce5ea47865c9ed25e277488bb0199d3
Sriram Vakkalanka <sriramvakkalanka@Mac.attlocal.net>
2025-07-27 18:06:11 -0500

    Add single-byte XOR challenge solution

--- challenges/set1.py
+++ challenges/set1.py
@@ -15,10 +15,61 @@ def xor(inputString, compareByte):
      compareByte = bytes.fromhex(compareByte)
 
      result = bytes([x^y for x,y in zip(inputByte, compareByte)])
-
-     
-
-
-     
      return result.hex()
     
+
+def findKey(hexString):
+     
+     byteSequence = bytes.fromhex(hexString)
+     eFrequencyList = []
+     eSum= 0
+     #ETAOIN SHRDLU
+
+     for i in range(256):
+         eSum= 0
+         for bit in byteSequence:
+            if (bit^i) == ord('e'):
+                eSum += 2
+            elif (bit^i) == (ord('t')):
+                eSum += 2
+            elif (bit^i) == (ord('a')):
+                eSum += 2
+            elif (bit^i) == (ord('o')):
+                eSum += 2
+            elif (bit^i) == (ord('i')):
+                eSum += 2
+            elif (bit^i) == (ord('n')):
+                eSum += 1
+            elif (bit^i) == (ord('s')):
+                eSum += 1
+            elif (bit^i) == (ord('h')):
+                eSum += 1
+            elif (bit^i) == (ord('r')):
+                eSum += 1
+            elif (bit^i) == (ord('d')):
+                eSum += 1
+            elif (bit^i) == (ord('l')):
+                eSum += 1
+            elif (bit^i) == (ord('u')):
+                eSum += 1
+        
+            
+
+
+         eFrequencyList.append(eSum)
+    
+     maxIndex =  (eFrequencyList.index(max(eFrequencyList)))
+
+     decryption = bytes([bit^maxIndex for bit in byteSequence])
+
+     return decryption
+
+
+
+
+
+
+          
+          
+
+
